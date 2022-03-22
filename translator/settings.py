4b8b8917c8d4bcbecf5fd5fb61092b71cba0c771
almahdibakkali96 <almahdibakkali96@gmail.com>
2022-03-22 03:25:20 +0100

    rollback -1

--- translator/settings.py
+++ translator/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # Add these new lines
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
