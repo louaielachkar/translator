00e73ef9b58d36ee08683e42e2b82e4e759d32af
almahdibakkali96 <almahdibakkali96@gmail.com>
2022-03-22 03:21:14 +0100

    test static

--- translator/settings.py
+++ translator/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # Add these new lines
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
